cd4208b31b8790761bdd7d4628b3e3e55fb0a8ee
Gerzsenyi Patrik <78800814+vyperrrr@users.noreply.github.com>
2024-05-16 12:59:58 +0200

    feat added unstyled header component

--- src/pages/layouts/RootLayout.tsx
+++ src/pages/layouts/RootLayout.tsx
@@ -1,7 +1,13 @@
 import { Outlet } from "react-router";
+import Header from "../../components/Header";
 
 const RootLayout: React.FC = () => {
-  return <Outlet />;
+  return (
+    <>
+      <Header />
+      <Outlet />
+    </>
+  );
 };
 
 export default RootLayout;
