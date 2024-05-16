7cd9306c889859ba2d1af0d3322c7bbff01557d2
Gerzsenyi Patrik <78800814+vyperrrr@users.noreply.github.com>
2024-05-16 12:41:49 +0200

    feat added routes accessible for unauthorized users

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,3 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 
-export const router = createBrowserRouter();
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "/login", element: <LoginPage /> }
+      { path: "/signup", element: <SignupPage /> }
+    ]
+  }
+]);
