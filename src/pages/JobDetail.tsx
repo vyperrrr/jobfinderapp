cebc583f05e593348c676cb0c79a47fb27d23df0
Gerzsenyi Patrik <78800814+vyperrrr@users.noreply.github.com>
2024-05-25 20:58:17 +0200

    feat now displaying success or error message after applying for a job

--- src/pages/JobDetail.tsx
+++ src/pages/JobDetail.tsx
@@ -1,8 +1,8 @@
 import { useParams } from "react-router";
 import { useGetJobQuery } from "../services/jobsApi";
-import { Button, Section } from "@radix-ui/themes";
-import { Table } from "@radix-ui/themes";
+import { Button, DataList, Section } from "@radix-ui/themes";
 import { formatSalary } from "../utils";
+import { toast } from "react-toastify";
 
 import { FileTextIcon } from "@radix-ui/react-icons";
 
@@ -11,9 +11,18 @@ import { useApplyForJobMutation } from "../services/applicantsApi";
 const JobDetail = () => {
   const { id } = useParams<{ id: string }>();
   const { data: job, isError, isLoading } = useGetJobQuery({ id });
-  const [applyForJob] = useApplyForJobMutation();
+  const [applyForJob, { isSuccess: isApplySuccess, isError: isApplyError }] =
+    useApplyForJobMutation();
 
-  const handleApply = () => applyForJob({ jobId: job!.id });
+  const handleApply = () => {
+    applyForJob({ jobId: job!.id });
+    if (isApplySuccess) {
+      toast.success("Sikeres jelentkezés!");
+    }
+    if (isApplyError) {
+      toast.error("Hiba történt a jelentkezés során.");
+    }
+  };
 
   if (isError) {
     return <div>An error occurred...</div>;
@@ -33,47 +42,42 @@ const JobDetail = () => {
       <div className="flex items-start justify-between">
         <span>
           <h1 className="text-4xl font-semibold">Cég részletei</h1>
-          <h2 className="text-2xl font-semibold">
-            Megtetszett a lehetőség? Jelentkezz!
-          </h2>
         </span>
         <Button variant="outline" onClick={handleApply}>
           <FileTextIcon />
           Jelentkezem a pozícióra
         </Button>
       </div>
-      <Table.Root>
-        <Table.Body>
-          <Table.Row>
-            <Table.RowHeaderCell>Név</Table.RowHeaderCell>
-            <Table.Cell>{job?.company}</Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.RowHeaderCell>Pozíció</Table.RowHeaderCell>
-            <Table.Cell>{job?.position}</Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.RowHeaderCell>Leiírás</Table.RowHeaderCell>
-            <Table.Cell>{job?.description}</Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.RowHeaderCell>Fizetési Sáv</Table.RowHeaderCell>
-            <Table.Cell>{salaryFormatted}</Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.RowHeaderCell>Foglalkoztatás típusa</Table.RowHeaderCell>
-            <Table.Cell>{job?.type}</Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.RowHeaderCell>Település</Table.RowHeaderCell>
-            <Table.Cell>{job?.city}</Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.RowHeaderCell>Home Office</Table.RowHeaderCell>
-            <Table.Cell>{hasHomeOffice}</Table.Cell>
-          </Table.Row>
-        </Table.Body>
-      </Table.Root>
+      <DataList.Root size="3">
+        <DataList.Item>
+          <DataList.Label minWidth="88px">Név</DataList.Label>
+          <DataList.Value>{job?.company}</DataList.Value>
+        </DataList.Item>
+        <DataList.Item>
+          <DataList.Label minWidth="88px">Pozíció</DataList.Label>
+          <DataList.Value>{job?.position}</DataList.Value>
+        </DataList.Item>
+        <DataList.Item>
+          <DataList.Label minWidth="88px">Leírás</DataList.Label>
+          <DataList.Value>{job?.description}</DataList.Value>
+        </DataList.Item>
+        <DataList.Item>
+          <DataList.Label minWidth="88px">Fizetési sáv</DataList.Label>
+          <DataList.Value>{salaryFormatted}</DataList.Value>
+        </DataList.Item>
+        <DataList.Item>
+          <DataList.Label minWidth="88px">Foglalkoztatás típusa</DataList.Label>
+          <DataList.Value>{job?.type}</DataList.Value>
+        </DataList.Item>
+        <DataList.Item>
+          <DataList.Label minWidth="88px">Település</DataList.Label>
+          <DataList.Value>{job?.city}</DataList.Value>
+        </DataList.Item>
+        <DataList.Item>
+          <DataList.Label minWidth="88px">Home Office</DataList.Label>
+          <DataList.Value>{hasHomeOffice}</DataList.Value>
+        </DataList.Item>
+      </DataList.Root>
     </Section>
   );
 };
